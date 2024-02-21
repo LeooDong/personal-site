739516bf64c3aed2b6510d9b9be10de3f85c88ab
Leo Dong <leo@Leos-MacBook-Pro.local>
2024-02-21 22:48:21 +0000

    updated all

---
+++ src/data/index.js
@@ -0,0 +1,9 @@
+const data = [
+  {
+    title: 'My Blog',
+    image: '/images/projects/blog.png',
+    link: 'https://blog.leodong.me',
+  },
+];
+
+export default data;
--- src/pages/Index.js
+++ src/pages/Index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import Main from '../layouts/Main';
+import Cell from '../components/Blog/Cell';
+import data from '../data/index';
 
 const Index = () => (
   <Main
@@ -21,6 +23,21 @@ const Index = () => (
         and <Link to="/art">art</Link> {' '} that I have made.
         Read some of my thoughts or things that piqued my interest <Link to="/blog">here</Link>. {' '}
       </p>
+    </article>
+
+    <article className="post" id="blog">
+      <header>
+        <div className="title">
+          <h2 data-testid="heading"><Link to="/blog">Blog</Link></h2>
+          <p>A selection of musings that might interest you</p>
+        </div>
+      </header>
+      {data.map((project) => (
+        <Cell
+          data={project}
+          key={project.title}
+        />
+      ))}
     </article>
   </Main>
 );
